408bbe641cb6bf3afe4d6ffed1944e3d12ed4277
gulshan07dev <gulshanku9135@gmail.com>
2023-09-05 22:01:02 +0530

    setup a currency app with all the configuration

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import React from "react"
+import CurrencyConvertor from "./pages/CurrencyConvertor"
+
+function App() { 
+
+  return (
+     <CurrencyConvertor />
+  )
+}
+
+export default App
---
+++ src/pages/CurrencyConvertor.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function CurrencyConvertor() {
+  return (
+     <section>
+        <h1>currency convertor app</h1>
+     </section>
+  )
+}
